d99528a2f13463a7e63ba2b979529e3a96dc3c79
milanseries <50550640+milanseries@users.noreply.github.com>
2022-07-14 10:40:05 +0545

    chore(miscellaneous): file extension change & remove default test

--- src/components/CustomSkeleton.jsx
+++ src/components/CustomSkeleton.jsx
@@ -5,13 +5,19 @@ const CustomSkeleton = ({ count = 1 }) => {
   return (
     <React.Fragment>
       {Array.from(new Array(count)).map((item, index) => (
-        <Card sx={{ marginBottom: '10px' }} key={index}>
+        <Card sx={{ marginBottom: '10px' }} key={index} elevation={6}>
           <Box sx={{ display: 'flex' }} elevation={6}>
-            <Skeleton variant='rectangular' width={120} height={80} sx={{ marginRight: '10px' }} />
+            <Skeleton
+              animation="wave"
+              variant="rectangular"
+              width={180}
+              height={120}
+              sx={{ marginRight: '10px' }}
+            />
             <Box sx={{ padding: '10px' }}>
-              <Skeleton variant='rectangular' width={200} height={12} />
+              <Skeleton variant="rectangular" width={200} height={12} />
               <br />
-              <Skeleton variant='rectangular' width={200} height={25} />
+              <Skeleton variant="rectangular" width={200} height={45} />
             </Box>
           </Box>
         </Card>
