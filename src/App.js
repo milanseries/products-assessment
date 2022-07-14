7366c9be2bcb7773032aaf2c598939fb3388fb83
milanseries <50550640+milanseries@users.noreply.github.com>
2022-07-14 10:42:08 +0545

    chore(styled): replaced with container styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
+import { Typography, Box } from '@mui/material';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Product from './components/Product';
+import { Container } from './styled/containerStyled';
 
 const queryClient = new QueryClient();
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div
-        style={{
-          display: 'flex',
-          padding: '32px',
-          alignItems: 'center',
-          minHeight: '90vh',
-          justifyContent: 'center',
-        }}
-      >
-        <Product />
-      </div>
+      <Container>
+        <Box>
+          <Typography variant="h2">Products list</Typography>
+          <Product />
+        </Box>
+      </Container>
     </QueryClientProvider>
   );
 }
